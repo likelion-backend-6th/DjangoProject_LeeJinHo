0d59f12774b5a2bd17430c7096c5a2885802442e
bainaryho <rkswl233@naver.com>
2023-07-17 11:20:02 +0900

    [추가]Status 필드, Status 클래스

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,10 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    class Status(models.TextChoices):  # TextChoices를 상속받는 열거형 클래스
+        DRAFT = 'DF', 'Draft'  # 게시물 상태 : 임시
+        PUBLISHED = 'PB', 'Published'  # 게시물 상태 : 게시됨
+
     # id = 자동생성(BigAutoField)
     title = models.CharField(max_length=250)  # 글의 제목을 위한 필드, SQL에서 VARCHAR열로 변환
     slug = models.SlugField(max_length=250)  # SlugField필드, 문자,숫자,밑줄,하이픈만 포함하는 짧은 레이블, SEO친화적인 URL구성
@@ -11,6 +15,11 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)  # datatime.now의 시간대를 고려한 버전
     created = models.DateTimeField(auto_now_add=True)  # 게시물 create 날짜 시간 저장, auto_now_add 객체를 생성할 때 날짜 저장
     updated = models.DateTimeField(auto_now=True)  # 게시물의 last update 날짜 저장, auto_now를 사용하여 객체 저장시 update
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
 
     class Meta:  # 메타 내부 클래스
         ordering = ['-publish']

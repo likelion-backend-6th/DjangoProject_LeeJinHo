619d09785654b6b6e6b204cd617978f86042d635
bainaryho <rkswl233@naver.com>
2023-07-18 11:02:55 +0900

    [추가]모델 매니저 생성

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,12 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset() \
+            .filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):  # TextChoices를 상속받는 열거형 클래스
         DRAFT = 'DF', 'Draft'  # 게시물 상태 : 임시
@@ -15,19 +21,23 @@ class Post(models.Model):
 
     author = models.ForeignKey(  # 장고가 관련모델의 기본키를 사용하여 데이터베이스에서 외래키를 생성함
         User,  # import한 User모델, M:1 관계, 사용자는 여러글을 작성한다
-        on_delete=models.CASCADE,  #객체가 삭제될 시 동작, 사용자가 삭제되면 글도 삭제된다
-        related_name='blog_posts', #user.blog_posts로 객체접근 가능
+        on_delete=models.CASCADE,  # 객체가 삭제될 시 동작, 사용자가 삭제되면 글도 삭제된다
+        related_name='blog_posts',  # user.blog_posts로 객체접근 가능
     )
 
     body = models.TextField()  # 글의 본문을 저장하는 필드, SQL에서 TEXT열로 변환
     publish = models.DateTimeField(default=timezone.now)  # datatime.now의 시간대를 고려한 버전
     created = models.DateTimeField(auto_now_add=True)  # 게시물 create 날짜 시간 저장, auto_now_add 객체를 생성할 때 날짜 저장
     updated = models.DateTimeField(auto_now=True)  # 게시물의 last update 날짜 저장, auto_now를 사용하여 객체 저장시 update
+
     status = models.CharField(
         max_length=2,
         choices=Status.choices,
         default=Status.DRAFT
     )
+
+    objects = models.Manager()  # 기본 매니저
+    published = PublishedManager()  # 사용자 정의 매니저
 
     class Meta:  # 메타 내부클래스
         ordering = ['-publish']

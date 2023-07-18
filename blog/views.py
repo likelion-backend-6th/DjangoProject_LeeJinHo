82519fed66fe11ed47cbf08649e102bfa0f1ddde
bainaryho <rkswl233@naver.com>
2023-07-18 11:03:11 +0900

    [추가]게시글 목록 뷰 생성

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,21 @@
-#어플리케이션의 로직이 들어갑니다.각 뷰는 HTTP요청을 받아 처리하고 응답을 반환
+# 어플리케이션의 로직이 들어갑니다.각 뷰는 HTTP요청을 받아 처리하고 응답을 반환
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+# def post_detail(request, id):
+#    try:
+#        post = Post.published.get(id=id)
+#    except Post.DoesNotExist:
+#        raise Http404("No Post found.")
+#    return render(request, 'blog/post/detail.html', {'post': post})
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post})

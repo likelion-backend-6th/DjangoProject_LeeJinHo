763c3d91f16e6b5e84344aeab1db9d458b78adc7
bainaryho <rkswl233@naver.com>
2023-07-19 11:26:43 +0900

    [추가]pagenation 오류처리

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,22 @@
 # 어플리케이션의 로직이 들어갑니다.각 뷰는 HTTP요청을 받아 처리하고 응답을 반환
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3) # 페이지당 3개의 게시물로 페이지네이션
-    page_number = request.GET.get('page', 1)
-
-    posts = paginator.page(page_number)
+    page_number = request.GET.get('page')
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # page_number가 정수가 아닌 경우 첫 번째 페이지 제공
+        posts = paginator.page(1)
+    except EmptyPage:
+        # page_number가 범위를 벗어난 경우 결과의 마지막 페이지 제공
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 

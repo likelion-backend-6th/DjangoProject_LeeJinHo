1d2c062a30cc9838b92730834d79c5d7fdff3669
bainaryho <rkswl233@naver.com>
2023-07-19 15:15:19 +0900

    [추가]share urls, views,html 추가

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     # post views
-    #path('', views.post_list, name='post_list'), 이전 URL 패턴
-    path('', views.PostListView.as_view(), name='post_list'), #PostListView 클래스 사용
+    # path('', views.post_list, name='post_list'), 이전 URL 패턴
+    path('', views.PostListView.as_view(), name='post_list'),  # PostListView 클래스 사용
     re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$', views.post_detail,
             name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 # 어플리케이션의 로직이 들어갑니다.각 뷰는 HTTP요청을 받아 처리하고 응답을 반환
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
@@ -44,15 +45,22 @@ def post_detail(request, year, month, day, post):
 
 
 def post_share(request, post_id):
-    # id로 글 검색
+    # id로 게시물 조회
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
-        # 폼이 제출되었습니다.
+        # 폼이 제출되었을 때
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            # 폼 필드가 유효한 경우
-            cd = form.cleaned_data #유효성 검사된 데이터 검색
-            # ... 이메일 전송
-    else: #폼이 유효하지 않음
+            # 폼 필드가 유효성 검사를 통과
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']}님이 {post.title}을(를) 추천합니다."
+            message = f"{post.title}을(를) 다음에서 읽어보세요.\n\n" \
+                      f"{cd['name']}의 의견: {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com', [cd['to']])
+        sent = True
+    else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent, })

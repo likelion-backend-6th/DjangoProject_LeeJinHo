14f0ade019a4431c94f845509d5ca4dc7b499636
bainaryho <rkswl233@naver.com>
2023-07-19 13:12:32 +0900

    [수정]클래스 뷰로 수정(PostListView 추가)

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),
+    #path('', views.post_list, name='post_list'), 이전 URL 패턴
+    path('', views.PostListView.as_view(), name='post_list'), #PostListView 클래스 사용
     re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$', views.post_detail,
             name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,24 @@
 # 어플리케이션의 로직이 들어갑니다.각 뷰는 HTTP요청을 받아 처리하고 응답을 반환
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+class PostListView(ListView):
+    # 대체글 목록뷰
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 # Create your views here.
 def post_list(request):
     per_page = request.GET.get('per_page', 3)
     post_list = Post.published.all()
-    #paginator = Paginator(post_list, 3) # 페이지당 3개의 게시물로 페이지네이션
+    # paginator = Paginator(post_list, 3) # 페이지당 3개의 게시물로 페이지네이션
     paginator = Paginator(post_list, per_page, orphans=1)
     page_number = request.GET.get('page')
     try:

fa0f0a1801af7b6f4e346ad15f3ac431cf4de122
bainaryho <rkswl233@naver.com>
2023-07-19 11:22:34 +0900

    [추가]pagenation 기능추가

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,7 @@
 #카탈로그 애플리케이션의 주요 설정을 포함합니다
 from django.apps import AppConfig
 
+
 class BlogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "blog"
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,21 @@
 # 어플리케이션의 로직이 들어갑니다.각 뷰는 HTTP요청을 받아 처리하고 응답을 반환
 from django.shortcuts import render, get_object_or_404
-from blog.models import Post
+from .models import Post
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3) # 페이지당 3개의 게시물로 페이지네이션
+    page_number = request.GET.get('page', 1)
+
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
-    #날짜 및 post인수를 받아와 주어진 슬러그와 출판일을 가진 게시글 검색
+    # 날짜 및 post인수를 받아와 주어진 슬러그와 출판일을 가진 게시글 검색
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
                              slug=post,
                              publish__year=year,

0dfeefafef043ee46fd3affd75e60e3617e370ff
bainaryho <rkswl233@naver.com>
2023-07-20 17:46:20 +0900

    [수정]페이지네이션 기능 클래스형 뷰 사용 가능하게 수정

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('list/', views.post_list, name='post_list'),
-    path('', views.post_list, name='post_list'), #함수형 뷰로 만드니까 페이지네이션 작동
-    #path('', views.PostListView.as_view(), name='post_list'), #PostListView 클래스 사용
+    #path('', views.post_list, name='post_list'), #함수형 뷰로 만드니까 페이지네이션 작동
+    path('', views.PostListView.as_view(), name='post_list'), #PostListView 클래스 사용
     re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$', views.post_detail,
             name='post_detail'),
 #5. URL패턴 생성
